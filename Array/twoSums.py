217a1059705a844fbed4402daeaaa0239d56e808
edinabwari <bwarikwambokaedina@gmail.com>
2023-09-28 18:32:51 +0300

    Made corrections on the file

--- Array/twoSums.py
+++ Array/twoSums.py
@@ -2,12 +2,18 @@
 #return indices of the two numbers such that they add up to target.
 # Each input should have one solution and elements shoud not be repeated twice.
 nums_array = [2,3,4,5]
-target = 10
+target = 8
+pairs = False
 for  i in range(len(nums_array)):
     for j in range(i + 1, len(nums_array)):
         if nums_array[i] + nums_array[j] == target:
+            pairs = True
             print(f"numbers are {nums_array[i]} + {nums_array[j]} = {target}")
-        else:
-            print("Invalid set of numbers.")
+if not pairs:          
+        print("Invalid set of numbers")
+        
+
+
+            
     
     
